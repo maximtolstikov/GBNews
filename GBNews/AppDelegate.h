0738862e93ec59cb342872fc9344245f82003f76
Maxim Tolstikov <vadimovich@list.ru>
2018-09-25 09:25:09 +0300

    init commit

---
+++ GBNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GBNews
+//
+//  Created by Maxim Tolstikov on 25/09/2018.
+//  Copyright © 2018 Maxim Tolstikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GBNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GBNews
+//
+//  Created by Maxim Tolstikov on 25/09/2018.
+//  Copyright © 2018 Maxim Tolstikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
